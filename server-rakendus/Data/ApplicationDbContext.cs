90033906bec8cb92dbeebaab2cc8b8836b3bd145
VirgoFrank <virgo.frank@tptlive.ee>
2020-03-04 09:58:57 +0200

    tunnitöö

---
+++ server-rakendus/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Data.Migrations;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<WebApplication1.Data.Migrations.Users> Users { get; set; }
+    }
+}
---
+++ server-rakendus/Data/Tasks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Data
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
